c770015c6a324b54909e110553843f25b05e74aa
Dennis Wethmar <dennis.wethmar@energyzero.nl>
2024-10-04 10:32:44 +0200

    Added camera

--- systems/render/render.go
+++ systems/render/render.go
@@ -24,6 +24,8 @@ type System struct {
 	logger  *slog.Logger
 	sprites map[sprite.Graphic]*Sprite
 	ecs     *ecsys.ECS
+	offsetX int
+	offsetY int
 }
 
 func New(
@@ -39,6 +41,8 @@ func New(
 		logger:  logger,
 		sprites: spriteMap,
 		ecs:     ecs,
+		offsetX: 0,
+		offsetY: 0,
 	}
 }
 
@@ -48,9 +52,22 @@ type entityDraw struct {
 	DrawFunc func(screen *ebiten.Image)
 }
 
-// Draw draws the shapes.
+// Draw draws the entities on the screen.
 func (s *System) Draw(screen *ebiten.Image) error {
 	entitiesToDraw := []entityDraw{}
+
+	// Draw checkered background
+	for x := 0; x < 800; x += 40 {
+		for y := 0; y < 600; y += 40 {
+			dx := float32(x - s.offsetX)
+			dy := float32(y - s.offsetY)
+			if (x/40+y/40)%2 == 0 {
+				vector.DrawFilledRect(screen, dx, dy, 40, 40, color.RGBA{R: 0x00, G: 0x00, B: 0x00, A: 0xff}, true)
+			} else {
+				vector.DrawFilledRect(screen, dx, dy, 40, 40, color.RGBA{R: 0x22, G: 0x22, B: 0x22, A: 0xff}, true)
+			}
+		}
+	}
 
 	// Collect rectangles to draw
 	for _, r := range s.ecs.Rectangles() {
@@ -65,7 +82,8 @@ func (s *System) Draw(screen *ebiten.Image) error {
 		entitiesToDraw = append(entitiesToDraw, entityDraw{
 			X: X, Y: Y,
 			DrawFunc: func(screen *ebiten.Image) {
-				vector.DrawFilledRect(screen, float32(X), float32(Y), float32(r.Width), float32(r.Height), color.RGBA{R: 0xff, G: 0x00, B: 0x00, A: 0xff}, true)
+				// Subtract the offset to correctly position the entity relative to the camera
+				vector.DrawFilledRect(screen, float32(X)-float32(s.offsetX), float32(Y)-float32(s.offsetY), float32(r.Width), float32(r.Height), color.RGBA{R: 0xff, G: 0x00, B: 0x00, A: 0xff}, true)
 			},
 		})
 	}
@@ -82,7 +100,7 @@ func (s *System) Draw(screen *ebiten.Image) error {
 		if !ok {
 			return fmt.Errorf("sprite not found: %s", spc.Graphic)
 		}
-		// apply offset
+		// Apply offset to the sprite
 		X += int64(spr.OffsetX)
 		Y += int64(spr.OffsetY)
 
@@ -91,13 +109,14 @@ func (s *System) Draw(screen *ebiten.Image) error {
 			X: X, Y: Y,
 			DrawFunc: func(screen *ebiten.Image) {
 				op := &ebiten.DrawImageOptions{}
-				op.GeoM.Translate(float64(X), float64(Y))
+				// Subtract the offset to correctly position the sprite relative to the camera
+				op.GeoM.Translate(float64(X)-float64(s.offsetX), float64(Y)-float64(s.offsetY))
 				screen.DrawImage(spr.Img, op)
 			},
 		})
 	}
 
-	// Sort entities by their Y value
+	// Sort entities by their Y value to render them correctly
 	sort.Slice(entitiesToDraw, func(i, j int) bool {
 		return entitiesToDraw[i].Y < entitiesToDraw[j].Y
 	})
@@ -111,5 +130,24 @@ func (s *System) Draw(screen *ebiten.Image) error {
 }
 
 func (s *System) Update() error {
+	controllables := s.ecs.Controllables()
+	if len(controllables) > 0 {
+		// Get the first controllable entity
+		firstControllable := controllables[0]
+
+		// Get the position of the controllable
+		pos, err := s.ecs.Position(firstControllable.Entity())
+		if err != nil {
+			return err
+		}
+
+		// Get the actual screen dimensions from Ebiten
+		screenWidth, screenHeight := ebiten.WindowSize()
+
+		// Calculate the offsets to center the controllable on the screen
+		s.offsetX = int(float64(pos.X) - float64(screenWidth)/2)
+		s.offsetY = int(float64(pos.Y) - float64(screenHeight)/2)
+	}
+
 	return nil
 }
